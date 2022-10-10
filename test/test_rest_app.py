1ac7a610dd3b7034a7c37b86262e822fff20b8be
Mwen-A <j.azugbene@hotmail.com>
2022-10-10 17:46:17 +0200

    spacing
    Signed-off-by: Mwen-A <j.azugbene@hotmail.com>

--- test/test_rest_app.py
+++ test/test_rest_app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 def test_request_times_get():
     with app.test_request_context('/', method='GET'):
-            with patch('app.set_current_time', return_value='10:05'):
-                expected = rest_app.request_times()
-                assert request.path == '/'
-                assert expected == '{"payload_message": "Five past ten"}'
+        with patch('app.set_current_time', return_value='10:05'):
+            expected = rest_app.request_times()
+            assert request.path == '/'
+            assert expected == '{"payload_message": "Five past ten"}'
 
 
 def test_request_times_post():
